1026e616646641b582600c25a00b18bbcf1e4672
mbregg <matthewbregg@gmail.com>
2016-04-22 01:40:48 -0400

    Added some traffic

---
+++ Project/Assets/TrafficTweenStartScriptA.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrafficTweenStartScriptA : MonoBehaviour {
+	public float delay = 0f;
+	// Use this for initialization
+	void Start () {
+
+			iTween.MoveTo (gameObject, iTween.Hash ("path", iTweenPath.GetPath ("PathA"), "time", 15, "orienttopath", true, "looptype", "loop", "easetype","linear","delay",delay));
+			
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
