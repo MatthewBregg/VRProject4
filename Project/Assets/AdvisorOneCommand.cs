04b099f93377970be9e4f0d349bf82d74b444067
mbregg <matthewbregg@gmail.com>
2016-04-24 21:52:41 -0400

    Added script to the people

---
+++ Project/Assets/AdvisorOneCommand.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdvisorOneCommand : MonoBehaviour, Command {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void executeCommand() {
+		Debug.Log ("Advisor one ran command!");
+	}
+}
---
+++ Project/Assets/AdvisorTwoCommand.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdvisorTwoCommand : MonoBehaviour, Command {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void executeCommand() {
+		Debug.Log ("Advisor two ran command!");
+	}
+}
