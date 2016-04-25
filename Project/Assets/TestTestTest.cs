57ad7a91a6a2c2f898c2987d59752f9940efb082
mbregg <matthewbregg@gmail.com>
2016-04-25 01:25:07 -0400

    OOps, one of the animations still waSN'T SET TO EXIT

---
+++ Project/Assets/TestTestTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestTestTest : MonoBehaviour {
+	public AdvisorAnimationEnum ani;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.GetComponent<AdvisorAnimationController>()._currentlyPlaying = ani;
+	}
+}
