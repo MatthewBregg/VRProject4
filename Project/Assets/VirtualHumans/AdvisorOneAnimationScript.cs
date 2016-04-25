56f90ebc200474b8e52186574fb6eddd3b2e05c9
mbregg <matthewbregg@gmail.com>
2016-04-25 00:04:36 -0400

    animations set up for characters on boolean values

---
+++ Project/Assets/VirtualHumans/AdvisorOneAnimationScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AdvisorOneAnimationScript : MonoBehaviour {
+
+	public Animator animatorControllerVar;
+	public bool isShakingHands = false;
+
+	int soundFileNumber = 0;
+	float[] soundFileTimes = new float[2] {1, 2};
+
+
+	void Start() {
+		animatorControllerVar = GetComponent<Animator> ();
+	}
+
+	void Update () {
+		if (isShakingHands == true) { 
+			playShakeHandsAnimation ();
+		}
+	}
+
+
+	void playShakeHandsAnimation() {
+		animatorControllerVar.SetTrigger("isShakingHands");
+	}
+
+}
+		
