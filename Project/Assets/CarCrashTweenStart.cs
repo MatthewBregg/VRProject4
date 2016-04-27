6581495d45ce11195a8679da6b5f8c112974aa5c
reidgill <=>
2016-04-27 04:57:39 -0400

    more stuff

---
+++ Project/Assets/CarCrashTweenStart.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarCrashTweenStart : MonoBehaviour {
+
+	public float delay = 0f;
+	public string pathName;
+	public float time = 15f;
+	public string type = "linear";
+	// Use this for initialization
+	void Start () {
+
+		iTween.MoveTo (gameObject, iTween.Hash ("path", iTweenPath.GetPath (pathName), "time", time, "orienttopath", true, "easetype",type,"delay",delay));
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
---
+++ Project/Assets/SwapCarsOnCollision.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwapCarsOnCollision : MonoBehaviour {
+	public GameObject before;
+	public GameObject after;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter(Collider other) {
+		if ( other.gameObject.CompareTag("TreeToCrashInto") ) {
+			before.SetActive(false);
+			after.SetActive(true);
+		}
+	}
+}
