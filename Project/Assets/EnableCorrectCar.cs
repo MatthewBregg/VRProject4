18491f63c74b96434f3f40a53dfca37ca4a462e9
reidgill <=>
2016-04-27 03:57:26 -0400

    Blocked off traffic

---
+++ Project/Assets/EnableCorrectCar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableCorrectCar : MonoBehaviour {
+	public GameObject taxiCar;
+	public GameObject uberCar;
+	// Use this for initialization
+	public bool tookTaxi;
+	private bool _tookTaxi;
+	void Start () {
+		tookTaxi = StaticDecisionsMade.chooseToTakeTaxi;
+		_tookTaxi = tookTaxi;
+		uberCar.SetActive (!tookTaxi);
+		taxiCar.SetActive (tookTaxi);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_tookTaxi != tookTaxi) {
+			_tookTaxi = tookTaxi;
+			uberCar.SetActive (!tookTaxi);
+			taxiCar.SetActive (tookTaxi);
+		}
+	}
+}
