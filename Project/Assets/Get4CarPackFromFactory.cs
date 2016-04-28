d16b2b11da121b837bfc3910757c70c1684ae003
reidgill <=>
2016-04-28 00:18:48 -0400

    taxis populate properly now

--- Project/Assets/Get4CarPackFromFactory.cs
+++ Project/Assets/Get4CarPackFromFactory.cs
@@ -5,15 +5,19 @@ public class Get4CarPackFromFactory : MonoBehaviour {
 	public Vector3 position;
 	public CarFactory factory;
 
+	bool switchBool = false;
 
 	// Use this for initialization
 	void Start () {
-		GameObject carPack = factory.getFourPackOfCars ();
-		factory.setParent (carPack, gameObject.transform, position);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (factory.allowedToSwitch && !switchBool) {
+			GameObject carPack = factory.getFourPackOfCars ();
+			factory.setParent (carPack, gameObject.transform, position);
+			switchBool = true;
+		}
 	}
 }
--- Project/Assets/Get6CarPackFromFactory.cs
+++ Project/Assets/Get6CarPackFromFactory.cs
@@ -4,19 +4,19 @@ using System.Collections;
 public class Get6CarPackFromFactory : MonoBehaviour {
 	public Vector3 position = new Vector3(0,0,0);
 	public CarFactory factory;
-
+	bool switchBool = false; 
 
 	// Use this for initialization
 	void Start () {
-
-		//while (!factory.allowedToSwitch) {}
-			GameObject carPack = factory.getSixPackOfCars ();
-			factory.setParent (carPack, gameObject.transform, position);
 
 	}
 
 	// Update is called once per frame
 	void Update () {
-
+		if (factory.allowedToSwitch && !switchBool) {
+			GameObject carPack = factory.getSixPackOfCars ();
+			factory.setParent (carPack, gameObject.transform, position);
+			switchBool = true;
+		}
 	}
 }
